b3826bc8be26cd506d78a474155b034b4aace520
Diana Rojas <diana.milenarb@gmail.com>
2020-07-14 12:15:39 -0500

    desarrolle el punto 1 y 2 del reto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+  if(true){
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+	    
     }, time);
+   }else{
+     reject(error(`=== Pedido para la ${table} de ${product}, no salió`)
+   }	  
   });
-}
+};
+
+function randomTime() = { 
+  const max = 8000;
+  const min = 1000;
+  return Math.floor(Math.random()*(max - min + 1) + min);
+};
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -16,9 +27,18 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0]).then((res) => console.log(res));
+	return orders(randomTime(), menu.pizza, table[2]).then((res) => console.log(res));
+    .catch((err) => console.error(err));
+};
+
+waiter2();
